2a1a065c704d47dd573b21072f96ab8b3d577f16
Vasil Yanakiev <Vasil.Yanakiev@docuware.com>
2025-04-23 10:28:45 +0300

    Add project files.

---
+++ ExpressionProcessing/ExpressionProcessor.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.VisualBasic;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace DocuWare.WorkflowDesigner.ExpressionProcessing
+{
+    public static partial class ExpressionProcessor
+    {
+        public static SemanticModel ProcessExpression()
+        {
+            var semanticModel = GetProcessingResult();
+            return semanticModel;
+
+        }
+
+        public static SemanticModel GetProcessingResult()
+        {
+
+            var codeTemplate = $@"
+                Option Strict On
+
+                Imports System
+                Imports Microsoft.VisualBasic
+                Imports Microsoft.VisualBasic.CompilerServices
+                Imports Microsoft.VisualBasic.Financial
+                Module Module1
+                    Sub TempWrapperFunction()
+123={{0}}
+                    End Sub
+
+                    Sub Main()
+                    End Sub
+                End Module
+                ";
+            var fullCode = string.Format(codeTemplate, "345");
+
+            var tree = VisualBasicSyntaxTree.ParseText(fullCode);
+            var compilation = VisualBasicCompilation.Create("TemporaryCompilation",
+                syntaxTrees: new[] { tree },
+                references: new[]
+                {
+                    MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                    MetadataReference.CreateFromFile(typeof(Microsoft.VisualBasic.Constants).Assembly.Location),
+                    MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location),
+                    MetadataReference.CreateFromFile(typeof(XElement).Assembly.Location),
+                    MetadataReference.CreateFromFile(typeof(System.Diagnostics.Debug).Assembly.Location),
+                    MetadataReference.CreateFromFile(typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly.Location),
+                    MetadataReference.CreateFromFile(Assembly.Load("System.Runtime, Version=6.0.0.0").Location),
+                });
+
+            var semanticModel = compilation.GetSemanticModel(tree);
+            return semanticModel;
+        }
+    }
+}
---
+++ RoslynWebAssembly/Program.cs
@@ -0,0 +1,21 @@
+﻿using DocuWare.WorkflowDesigner.ExpressionProcessing;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace RoslynWebAssembly
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetCompiledResult());
+        }
+
+        [JSExport]
+        static bool GetCompiledResult()
+        {
+
+            var expressionOutcome = ExpressionProcessor.ProcessExpression();
+            return expressionOutcome.IsSpeculativeSemanticModel;
+        }
+    }
+}
